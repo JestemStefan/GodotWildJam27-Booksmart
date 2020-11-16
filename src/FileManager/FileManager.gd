2f5100843dabd2d8f2fb58e0d47eea99c6929d4b
IAmNotIsaac <isaacjulianastell@gmail.com>
2020-11-16 04:14:04 -0800

    Removed level editor and level manager from project. No longer served useful purpose.

--- src/FileManager/FileManager.gd
+++ src/FileManager/FileManager.gd
@@ -15,19 +15,3 @@ func load_from_json(file_path : String): # Uknown return type
 	file.open(file_path, File.READ)
 	var content = str2var(file.get_as_text())
 	return content
-
-
-
-func str2vec3(string : String) -> Vector3:
-	var register := ""
-	for character in string:
-		if character in "-0123456789,":
-			register += character
-	
-	var split := register.split(",")
-	
-	var x = int(split[0])
-	var y = int(split[1])
-	var z = int(split[2])
-	
-	return Vector3(x, y, z)
