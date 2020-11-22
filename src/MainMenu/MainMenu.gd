b44e598372ed25d0c0e3c3d82e9e902cd76f45e6
IAmNotIsaac <isaacjulianastell@gmail.com>
2020-11-22 06:11:55 -0800

    Added credits menu and slightly modified main menu.

--- src/MainMenu/MainMenu.gd
+++ src/MainMenu/MainMenu.gd
@@ -1,6 +1,6 @@
 extends Spatial
 
-enum Focuses { MAIN, OPTIONS, CREDITS, QUIT }
+enum Focuses { MAIN, OPTIONS, CREDITS }
 
 var focus : int = Focuses.MAIN
 
@@ -13,7 +13,9 @@ onready var paige_skele = $Paige/Librarian/Skeleton
 onready var paige_books = $Paige/Books
 onready var fade_anim = $Overlay/AnimationPlayer
 onready var overlay = $Overlay
+onready var main = $Main
 onready var settings = $Settings
+onready var credits = $Credits
 
 
 
@@ -21,6 +23,7 @@ func _ready() -> void:
 	camera.translation = camera_points.get_node("0").translation
 	camera.rotation_degrees = camera_points.get_node("0").rotation_degrees
 	
+	credits.visible = false
 	settings.visible = false
 	overlay.visible = false
 	paige_anim.play("Idle_Books")
@@ -35,6 +38,7 @@ func _process(_delta) -> void:
 
 
 func change_menu(menu : int) -> void:
+	main.visible = false
 	match focus:
 		0:
 			match menu:
@@ -44,7 +48,10 @@ func change_menu(menu : int) -> void:
 					change_camera_view(1)
 					focus = Focuses.OPTIONS
 					settings.visible = true
-				2: pass
+				2: 
+					change_camera_view(2)
+					focus = Focuses.CREDITS
+					credits.visible = true
 				3:
 					fade_anim.play("fade_black")
 					focus = Focuses.QUIT
@@ -55,6 +62,15 @@ func change_menu(menu : int) -> void:
 					change_camera_view(0)
 					focus = Focuses.MAIN
 					settings.visible = false
+					main.visible = true
+		
+		2:
+			match menu:
+				0:
+					change_camera_view(0)
+					focus = Focuses.MAIN
+					credits.visible = false
+					main.visible = true
 
 
 
